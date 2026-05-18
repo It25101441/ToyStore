5c79fd2356673b1ae9fd28e434989dd1830bd0e3
IT25101224 <it25101224@my.sliit.lk>
2026-05-18 18:43:51 +0530

    erroe fixed

--- src/main/java/com/toystore/dao/MessageDAO.java
+++ src/main/java/com/toystore/dao/MessageDAO.java
@@ -1,4 +1,12 @@
+package com.toystore.dao;
 
+import com.toystore.model.Message;
+import com.toystore.model.Notification;
+import com.toystore.model.User;
+import com.toystore.utils.DBConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageDAO {
 
